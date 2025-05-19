36b441953d39b293b44f2b90f566c84e2f38d4a3
felipeGarzonMelguizo <69770704+Flying-Spaghetti-Monster69@users.noreply.github.com>
2025-05-19 16:56:19 -0500

    ping-pong resuelto

--- ping-pong/starter.js
+++ ping-pong/starter.js
@@ -1,8 +1,68 @@
 //1. Acciones a los botones
+
+let player1Score = 0;
+let player2Score = 0;
+
+const addPointToPlayer1 = document.getElementById("p1Button");
+const addPointToPlayer2 = document.getElementById("p2Button");
+const resetGame = document.getElementById("reset");
+const maxValue = document.getElementById("playto");
+const p1Display = document.getElementById("p1Display");
+const p2Display = document.getElementById("p2Display");
+
 //2. Botón verde asociado a jugador 1
+
+addPointToPlayer1.addEventListener("click", () => {
+  player1Score++;
+  p1Display.textContent = player1Score;
+  checkWinner();
+});
+
 //3. Botón rojo asociado a jugador 2
+
+addPointToPlayer2.addEventListener("click", () => {
+  player2Score++;
+  p2Display.textContent = player2Score;
+  checkWinner();
+});
+
 //4. Botón de reset
+
+resetGame.addEventListener("click", reset);
+
 //5. Select de cantidad de puntos a jugar
+
+maxValue.addEventListener("change", reset);
+
 //6. Lógica de juego
 
-// Aquí comienza tu código
+function reset() {
+  player1Score = 0;
+  player2Score = 0;
+  p1Display.textContent = player1Score;
+  p2Display.textContent = player2Score;
+  toggleButtons(false);
+  setColors("black", "black");
+}
+
+function checkWinner() {
+  const winningScore = maxValue.value;
+  if (player1Score >= winningScore) {
+    toggleButtons(true);
+    setColors("green", "red");
+  }
+  if (player2Score >= winningScore) {
+    toggleButtons(true);
+    setColors("red", "green");
+  }
+}
+
+function toggleButtons(boolean) {
+  addPointToPlayer1.disabled = boolean;
+  addPointToPlayer2.disabled = boolean;
+}
+
+function setColors(p1Color, p2Color) {
+  p1Display.style.color = p1Color;
+  p2Display.style.color = p2Color;
+}
